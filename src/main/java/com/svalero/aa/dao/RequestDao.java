a35d71192afde0da74eb1baf80de6c71385e02b6
Javier Martinez <javiermg90@gmail.com>
2024-05-20 19:56:40 +0200

    arreglado nombre tabla

--- src/main/java/com/svalero/aa/dao/RequestDao.java
+++ src/main/java/com/svalero/aa/dao/RequestDao.java
@@ -8,23 +8,23 @@ import org.jdbi.v3.sqlobject.statement.UseRowMapper;
 import java.util.List;
 
 public interface RequestDao {
-    @SqlQuery("SELECT * FROM requests")
+    @SqlQuery("SELECT * FROM request")
     @UseRowMapper(RequestMapper.class)
     List<Request> getAllRequests();
 
-    @SqlQuery("SELECT * FROM requests WHERE user_id = ?")
+    @SqlQuery("SELECT * FROM request WHERE user_id = ?")
     @UseRowMapper(RequestMapper.class)
     List<Request> getRequestsByUser(int user_id);
 
-    @SqlQuery("SELECT * FROM requests WHERE dog_id = ?")
+    @SqlQuery("SELECT * FROM request WHERE dog_id = ?")
     @UseRowMapper(RequestMapper.class)
     List<Request> getRequestsByDog(int dog_id);
 
-    @SqlQuery("SELECT * FROM requests WHERE dog_id LIKE CONCAT('%',:searchTerm,'%') OR user_id LIKE CONCAT('%',:searchTerm,'%') OR req_text LIKE CONCAT('%',:searchTerm,'%')")
+    @SqlQuery("SELECT * FROM request WHERE dog_id LIKE CONCAT('%',:searchTerm,'%') OR user_id LIKE CONCAT('%',:searchTerm,'%') OR req_text LIKE CONCAT('%',:searchTerm,'%')")
     @UseRowMapper(RequestMapper.class)
     List<Request> getSearchRequest(@Bind("searchTerm") String searchTerm);
 
-    @SqlUpdate("INSERT INTO requests(dog_id, user_id, req_text) VALUES (?, ?, ?)")
+    @SqlUpdate("INSERT INTO request(dog_id, user_id, req_text) VALUES (?, ?, ?)")
     int addRequest(int dog_id, int user_id, String req_text);
 
 }
